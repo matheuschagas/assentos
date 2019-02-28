573bbc5ebecff97e320748c940d83e488a8fdc1d
Matheus Chagas <matheus.chagas@uhuu.com>
2019-02-28 14:51:03 -0300

    :rocket: persist seat selection in memory

--- index.js
+++ index.js
@@ -2,7 +2,77 @@ var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(':assentos:');
 
+let booked = {};
+
+let c = console;
+
+
+function populateDatabase() {
+    //region create key value table
+    let sql = "CREATE TABLE IF NOT EXISTS keyvalue(" +
+        "key TEXT," +
+        "value TEXT" +
+        ")";
+
+    db.serialize(() => {
+        // queries will execute in serialized mode
+        db.run(sql);
+    });
+    //endregion
+};
+
+function bookSeat(msg){
+    try{
+        booked[msg.seat] = msg;
+       /* //region create key value table
+        let sql = "INSERT INTO keyvalue(key, value) VALUES ('map', " + JSON.stringify(booked) + ")";
+
+        db.serialize(() => {
+            // queries will execute in serialized mode
+            db.run(sql);
+        });*/
+        //endregion
+    }catch(e){
+        console.log(e);
+    }
+
+}
+
+function unbookSeat(msg){
+    try{
+        booked[msg.seat] = undefined;
+        //region create key value table
+        /*let sql = "INSERT INTO keyvalue(key, value) VALUES ('map', " + JSON.stringify(booked) + ")";
+
+        db.serialize(() => {
+            // queries will execute in serialized mode
+            db.run(sql);
+        });
+        //endregion*/
+    }catch(e){
+        console.log(e);
+    }
+}
+
+function getBookedSeats(){
+    return booked;
+}
+
+function resetDatabase(){
+    //region delete all database data
+    let sql = "DELETE FROM keyvalue";
+
+    db.serialize(() => {
+        // queries will execute in serialized mode
+        db.run(sql);
+    });
+    //endregion
+}
+
+populateDatabase();
 var clients = {};
 
 app.get('/', function(req, res){
@@ -19,13 +89,28 @@ io.on("connection", function (client) {
     client.on("join", function(name){
         console.log("Joined: " + name);
         clients[client.id] = name;
-        client.emit("update", "You have connected to the server.");
-        client.broadcast.emit("update", name + " has joined the server.")
+        client.emit("update", {type: 'message', message: "You have connected to the server."});
+        client.emit("update", {type: 'init', booked});
+        client.broadcast.emit("update", {type: 'message', message: "Joined: " + name})
     });
 
     client.on("send", function(msg){
-        console.log("Message: " + msg);
-        client.broadcast.emit("seatUpdate", clients[client.id], msg);
+        c.log("Message: " + msg);
+        switch (msg.action) {
+            case "deselectSeat":
+                unbookSeat(msg);
+                client.broadcast.emit("seatUpdate", clients[client.id], msg);
+                break;
+            case "selectSeat":
+                bookSeat(msg);
+                client.broadcast.emit("seatUpdate", clients[client.id], msg);
+                break;
+            case "clearDatabase":
+                resetDatabase();
+                booked = {};
+                client.emit("clearDatabase", clients[client.id], msg);
+                client.broadcast.emit("clearDatabase", clients[client.id], msg);
+        }
     });
 
     client.on("disconnect", function(){
