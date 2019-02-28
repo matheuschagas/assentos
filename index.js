6f38918aa7b0455579ee2fa06e018d24414456f4
matheuschagas <matheus.chagas@acad.pucrs.br>
2019-02-28 00:52:27 -0300

    :tada: first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+
+var clients = {};
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/src/index.html')
+});
+
+//SocketIO vem aqui
+
+http.listen(3003, function(){
+    console.log('listening on port 3003');
+});
+
+io.on("connection", function (client) {
+    client.on("join", function(name){
+        console.log("Joined: " + name);
+        clients[client.id] = name;
+        client.emit("update", "You have connected to the server.");
+        client.broadcast.emit("update", name + " has joined the server.")
+    });
+
+    client.on("send", function(msg){
+        console.log("Message: " + msg);
+        client.broadcast.emit("seatUpdate", clients[client.id], msg);
+    });
+
+    client.on("disconnect", function(){
+        console.log("Disconnect");
+        io.emit("update", clients[client.id] + " has left the server.");
+        delete clients[client.id];
+    });
+});
